1b49062a64b3d687e85d18fda10852661ffb5697
Grant Matai Cross <grantmatai@gmail.com>
2024-11-13 09:16:16 -0700

    feat:mobile view nav feat:(unfinished)non mobile view nav
    non-mobile view is not done... Still deciding exactly how I want it to look

---
+++ script/navscript.js
@@ -0,0 +1,5 @@
+// navscript.js
+document.getElementById('hamburger').addEventListener('click', function() {
+    this.classList.toggle('open');
+    document.getElementById('nav').classList.toggle('open');
+});
