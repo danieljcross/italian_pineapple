6afac88124d52dc25729fd233bb3d7c74348cc0f
Grant Matai Cross <grantmatai@gmail.com>
2024-11-19 10:53:20 -0700

    feat: about page & nav update
    updated the about page and fixed implementation of the nav in mobile view

--- script/navscript.js
+++ script/navscript.js
@@ -3,3 +3,5 @@ document.getElementById('hamburger').addEventListener('click', function() {
     this.classList.toggle('open');
     document.getElementById('nav').classList.toggle('open');
 });
+
+{/* <p class="about">Welcome to Pineapple Paradise, where the sunny sweetness of pineapple meets the savory perfection of pizza! Here at our vibrant eatery, we believe in celebrating the unexpected harmony between tropical fruit and pizza crust, bringing you a slice of sunshine with every bite. Whether you’re a devoted fan of the classic Hawaiian or you’re daring enough to try something new, we’ve crafted a menu full of deliciously bold combinations that elevate pineapple to its rightful place as a pizza topping champion. From sweet and smoky to tangy and savory, our pizzas are made with the finest ingredients, all prepared with a dash of creativity and a whole lot of love. So grab a slice, kick back, and let us transport you to the sweet, sunny side of pizza paradise!</p> */}
